cd1461f0460b9321354a00e50ba90753e1d59ec2
benshar <bhshar0@gmail.com>
2025-01-12 11:36:50 -0500

    Create model_training.py

---
+++ stock_price_project_1/version-3/src/model_training.py
@@ -0,0 +1,67 @@
+import matplotlib.pyplot as plt
+import math
+import numpy as np
+import copy
+
+
+def sigmoid(z):
+    g = 1 / (1 + np.exp(-z))
+
+    return g
+
+def cost_function(X, y, w, b):
+
+    m= X.shape[0]
+    total_cost = 0
+    for i in range(m):
+        z = np.dot(w,X[i]) + b
+        f_wb = sigmoid(z)
+        cost = -y[i]*np.log(f_wb) - (1-y[i])*np.log(1-f_wb)
+        total_cost += cost
+    total_cost = total_cost / m
+    return total_cost
+
+def gradient_calculation(X, y, w, b):
+
+    m,n = X.shape
+    dj_dw = np.zeros((n,))
+    dj_db = 0
+    for i in range(m):
+        z_i = np.dot(X[i],w) + b
+        f_wb = sigmoid(z_i)
+        for j in range(n):
+            dj_dw_i = (f_wb - y[i]) * X[i, j]
+            dj_dw[j] += dj_dw_i
+        dj_db_i = (f_wb - y[i])
+        dj_db += dj_db_i
+    dj_dw = dj_dw / m
+    dj_db = dj_db / m
+    return dj_dw, dj_db
+
+def gradient_descent(x, y, w_in, b_in, alpha, num_iters):
+    J_history = []
+    p_history = []
+    iteration_history = []
+    b = b_in
+    w = copy.deepcopy(w_in)
+
+    for i in range(num_iters):
+        dj_dw, dj_db = gradient_calculation(x, y, w, b)
+        w -= alpha * dj_dw
+        b -= alpha * dj_db
+
+        if i % 10 == 0 and i < 100000:
+            J_history.append(cost_function(x, y, w, b))
+            p_history.append([w, b])
+            iteration_history.append(i)
+        if i % math.ceil(num_iters / 10) == 0:
+            print(f"Iteration {i:4}: Cost {J_history[-1]:0.2e} ")
+
+    return w, b, J_history, p_history, iteration_history
+
+def plot_cost_over_iterations(J_history, iteration_history):
+    plt.plot(iteration_history, J_history, label='Cost over Iterations')
+    plt.xlabel('Iterations')
+    plt.ylabel('Cost')
+    plt.title('Cost over Iterations')
+    plt.show()
